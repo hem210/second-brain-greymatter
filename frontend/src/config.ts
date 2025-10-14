058c94e0aaf805cf8f42295d5dbfb25669206f08
hem210 <75677231+hem210@users.noreply.github.com>
2025-10-14 21:48:50 +0530

    feat: moved backend url to env

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,19 @@
-export const BACKEND_URL = "http://127.0.0.1:8000"
+import dotenv from "dotenv";
+
+// Load environment variables from .env
+dotenv.config();
+
+function getEnvVar(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is missing`);
+  }
+  return value;
+}
+
+export const BACKEND_URL = getEnvVar("BACKEND_URL");
 export const enum ContentType {
     Youtube="youtube",
     Twitter="twitter",
     Note="note"
-}
+};
